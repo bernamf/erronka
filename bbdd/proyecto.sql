05717ede9f08b552f8b2635697714d5084f8d0f8
JagobaPeruarena <ikcfe@plaiaundi.net>
2024-03-12 14:14:07 +0100

    PrimerCommite

---
+++ bbdd/proyecto.sql
@@ -0,0 +1,142 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 12-03-2024 a las 14:07:53
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `proyecto`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `arma`
+--
+
+CREATE TABLE `arma` (
+  `ID` int(11) NOT NULL,
+  `Nombre` varchar(100) DEFAULT NULL,
+  `Ataque` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `caballero`
+--
+
+CREATE TABLE `caballero` (
+  `ID` int(11) NOT NULL,
+  `Nombre` varchar(100) DEFAULT NULL,
+  `Arma_ID` int(11) DEFAULT NULL,
+  `Escudo_ID` int(11) DEFAULT NULL,
+  `Escudero_ID` int(11) DEFAULT NULL,
+  `idCaballo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `caballo`
+--
+
+CREATE TABLE `caballo` (
+  `ID` int(11) NOT NULL,
+  `Nombre` varchar(100) DEFAULT NULL,
+  `VelocidadMaxima` int(11) DEFAULT NULL,
+  `Resistencia` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `escudero`
+--
+
+CREATE TABLE `escudero` (
+  `ID` int(11) NOT NULL,
+  `Nombre` varchar(100) DEFAULT NULL,
+  `Experiencia` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `escudo`
+--
+
+CREATE TABLE `escudo` (
+  `ID` int(11) NOT NULL,
+  `Nombre` varchar(100) DEFAULT NULL,
+  `Defensa` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `arma`
+--
+ALTER TABLE `arma`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indices de la tabla `caballero`
+--
+ALTER TABLE `caballero`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `FK_Caballero_Arma` (`Arma_ID`),
+  ADD KEY `FK_Caballero_Escudo` (`Escudo_ID`),
+  ADD KEY `FK_Caballero_Escudero` (`Escudero_ID`),
+  ADD KEY `FK_Caballero_Caballo` (`idCaballo`);
+
+--
+-- Indices de la tabla `caballo`
+--
+ALTER TABLE `caballo`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indices de la tabla `escudero`
+--
+ALTER TABLE `escudero`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indices de la tabla `escudo`
+--
+ALTER TABLE `escudo`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `caballero`
+--
+ALTER TABLE `caballero`
+  ADD CONSTRAINT `FK_Caballero_Arma` FOREIGN KEY (`Arma_ID`) REFERENCES `arma` (`ID`),
+  ADD CONSTRAINT `FK_Caballero_Caballo` FOREIGN KEY (`idCaballo`) REFERENCES `caballo` (`ID`),
+  ADD CONSTRAINT `FK_Caballero_Escudero` FOREIGN KEY (`Escudero_ID`) REFERENCES `escudero` (`ID`),
+  ADD CONSTRAINT `FK_Caballero_Escudo` FOREIGN KEY (`Escudo_ID`) REFERENCES `escudo` (`ID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
