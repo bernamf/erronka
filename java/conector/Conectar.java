6db79d23542f06861534674b1b3b3269e4957686
benat merino <ikctr@plaiaundi.net>
2024-03-13 13:59:43 +0100

    mas clases

---
+++ java/conector/Conectar.java
@@ -0,0 +1,50 @@
+package conector;
+
+
+	import java.sql.Connection;
+	import java.sql.DriverManager;
+	import java.sql.SQLException;
+
+	public class Conectar {
+		protected Connection cn;
+
+		private String host = "localhost";
+		private String bbdd = "proyecto";
+		private String usuario = "root";
+		private String contrasenia = "";
+
+		public void conectar() {
+			try {
+				Class.forName("com.mysql.cj.jdbc.Driver");
+
+				this.cn = DriverManager.getConnection("jdbc:mysql://" + host + "/" + bbdd, usuario, contrasenia);
+
+
+			} catch (ClassNotFoundException e) {
+				System.out.println("Error al importar Driver");
+				e.printStackTrace();
+			} catch (SQLException e) {
+				System.out.println("Error conexion");
+				e.printStackTrace();
+			}
+		}
+
+		public void cerrar() {
+			try {
+				cn.close();
+			} catch (SQLException e) {
+				System.out.println("Error al cerrar la conexion");
+				e.printStackTrace();
+			}
+		}
+
+		public Connection getCn() {
+			return cn;
+		}
+
+		public void setCn(Connection cn) {
+			this.cn = cn;
+		}
+	}
+
+
