0469cd8c54107eef53090735ae40f5054ae54b79
benat merino <ikctr@plaiaundi.net>
2024-03-18 09:35:16 +0100

    armas y escudo terminado del todo

--- java/menu/Visor.java
+++ java/menu/Visor.java
@@ -17,16 +17,24 @@ public class Visor {
 		}
 	}
 	
-	public static void mostrarArmas(ArrayList<Arma> armas) {
-		for (Arma arma : armas) {
-			System.out.println(arma.toString());
-		}
-	}
-	
-	public static void mostrarEscudos(ArrayList<Escudo> escudos) {
-		for (Escudo escudo : escudos) {
-			System.out.println(escudo.toString());
-		}
-	}
+	 public static void mostrarArmas(ArrayList<Arma> armas) {
+	        System.out.println("Listado de Armas:");
+	        System.out.println("ID | Nombre              | Ataque");
+	        System.out.println("---------------------------------");
+	        for (Arma arma : armas) {
+	            System.out.printf("%-3d| %-20s| %-6d\n", arma.getId(), arma.getNombre(), arma.getAtaque());
+	        }
+	        System.out.println();
+	    }
+
+	    public static void mostrarEscudos(ArrayList<Escudo> escudos) {
+	        System.out.println("Listado de Escudos:");
+	        System.out.println("ID | Nombre              | Defensa");
+	        System.out.println("------------------------------------");
+	        for (Escudo escudo : escudos) {
+	            System.out.printf("%-3d| %-20s| %-8d\n", escudo.getId(), escudo.getNombre(), escudo.getDefensa());
+	        }
+	        System.out.println();
+	    }
 
 }
