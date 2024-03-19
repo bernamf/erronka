6ef5df096a276f4b305e7f88a3207df1edd14fc0
JagobaPeruarena <ikcfe@plaiaundi.net>
2024-03-19 08:36:49 +0100

    Modelos caballo/escudero

--- java/modelo/ModeloCaballo.java
+++ java/modelo/ModeloCaballo.java
@@ -1,7 +1,57 @@
 package modelo;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import clases.Caballo;
 import conector.Conectar;
 
 public class ModeloCaballo extends Conectar{
+	public Caballo getEscuderoById(int id) {
+		String query = "SELECT * from caballo WHERE id = ?";
+		
+		try (PreparedStatement st = getCn().prepareStatement(query)) {
+			st.setInt(1, id);
+			ResultSet rs = st.executeQuery();
+
+			if (rs.next()) {
+
+				Caballo caballo = new Caballo();
+				caballo.setId(rs.getInt("id"));
+				caballo.setResistencia(rs.getInt("resistencia"));
+				caballo.setNombre(rs.getString("nombre"));
+				caballo.setVelocidadMaxima(rs.getInt("velocidadMaxima"));
+				return caballo;
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+	public Caballo getEscuderoByIdCaba(int id) {
+		String query = "SELECT * from caballo WHERE idCaballero = ?";
+		
+		try (PreparedStatement st = getCn().prepareStatement(query)) {
+			st.setInt(1, id);
+			ResultSet rs = st.executeQuery();
+
+			if (rs.next()) {
+
+				Caballo caballo = new Caballo();
+				caballo.setId(rs.getInt("id"));
+				caballo.setResistencia(rs.getInt("resistencia"));
+				caballo.setNombre(rs.getString("nombre"));
+				caballo.setVelocidadMaxima(rs.getInt("velocidadMaxima"));
+				return caballo;
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
 
 }
--- java/modelo/ModeloEscudero.java
+++ java/modelo/ModeloEscudero.java
@@ -1,7 +1,56 @@
 package modelo;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import clases.Escudero;
 import conector.Conectar;
 
 public class ModeloEscudero extends Conectar{
+	public Escudero getEscuderoById(int id) {
+		String query = "SELECT * from escudero WHERE id = ?";
+		
+		try (PreparedStatement st = getCn().prepareStatement(query)) {
+			st.setInt(1, id);
+			ResultSet rs = st.executeQuery();
 
+			if (rs.next()) {
+
+				Escudero escudero = new Escudero();
+				escudero.setId(rs.getInt("id"));
+				escudero.setExperiencia(rs.getInt("experiencia"));
+				escudero.setNombre(rs.getString("nombre"));
+
+				return escudero;
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+	public Escudero getEscuderoByIdCaba(int id) {
+		String query = "SELECT * from escudero WHERE idCaballero = ?";
+		
+		try (PreparedStatement st = getCn().prepareStatement(query)) {
+			st.setInt(1, id);
+			ResultSet rs = st.executeQuery();
+
+			if (rs.next()) {
+
+				Escudero escudero = new Escudero();
+				escudero.setId(rs.getInt("id"));
+				escudero.setExperiencia(rs.getInt("experiencia"));
+				escudero.setNombre(rs.getString("nombre"));
+
+				return escudero;
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
 }
